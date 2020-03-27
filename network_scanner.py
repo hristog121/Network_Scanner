828801ad3164de1981e79fb3e11aa340e94700b1
root <root@kali-g770.lan>
2020-03-26 21:33:38 -0400

    Final scanner

--- network_scanner.py
+++ network_scanner.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python3
 
 import scapy.all as scapy
+import argparse
 
 
+# The actual scanner
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
+    clients_list = []
     for element in answered_list:
-        print(element[1].psrc)
-        print(element[1].hwsrc)
-        print("----------------------------------------------------")
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+
+    return clients_list
 
 
-scan("192.168.3.1/25")
+# Print the results as a table
+def print_result(results_list):
+    print("IP \t\t\tMAC Address\n---------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+
+# Parse an argument from the command line
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Target IP / IP range.")
+    (options) = parser.parse_args()
+    return options
+
+
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
